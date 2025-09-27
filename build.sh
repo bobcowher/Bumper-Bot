38c1861a034d1f3e26f71129b06b57641598149f
Robert Cowher <bobcowher@gmail.com>
2025-09-27 17:21:32 -0500

    making critical changes to disable controller launch and add build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+conda deactivate
+
+echo "Cleaning up project..."
+rm -f compile_commands.json
+
+echo "Building project..."
+
+PATH="/usr/bin:/home/robertcowher/rosprojects/bumperbot_ws/install/bumperbot_localization/bin:/opt/ros/jazzy/opt/gz_msgs_vendor/bin:/opt/ros/jazzy/opt/gz_tools_vendor/bin:/opt/ros/jazzy/opt/gz_ogre_next_vendor/bin:/opt/ros/jazzy/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/snap/bin"
+
+. install/setup.bash
+
+colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+	     --merge-install
+
+cp build/compile_commands.json .
+
+# Disable if robot isn't physically connected
+arduino-cli compile --fqbn arduino:avr:uno ./src/bumperbot_firmware/firmware/robot_control/
+arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano:cpu=atmega328old ./src/bumperbot_firmware/firmware/robot_control/
--- bumperbot_bringup/launch/real_robot.launch.py
+++ bumperbot_bringup/launch/real_robot.launch.py
@@ -94,7 +94,7 @@ def generate_launch_description():
         hardware_interface,
         laser_driver,
         controller,
-        joystick,
+    #    joystick,
         imu_driver_node,
         localization,
         slam,
